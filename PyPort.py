67c1147eee29d4998a6af84daa9d44278109f091
wanto-h <PedroRib.Carv@tutamail.com>
2025-01-10 07:32:33 +0000

    Update PyPort.py
    Made it work by fixing local and global variables

--- PyPort.py
+++ PyPort.py
@@ -20,24 +20,32 @@ scanmode=int(input("Type the number of the scan: "))
 #Adds everything to the queue
 class scan():
     def __init__(self,startport,endport):
+        port=1
         for port in range(startport, endport):
+            port+=1
             queue.put(port)
-
-#Defines which ports to add in the queue based on the mode
-if scanmode==1:
-   scan(allportstart,allportend)
-elif scanmode==2:
-    scan(mainportstart,mainportend)
-else:
-    print("This mode does not exist")
 
 #Scans every port and returns open ports
 def portscan():
-    for port in queue.get():
-        s=socket(socket.AF_INET,socket.SOCK_STREAM)
+    global open_ports
+    while not queue.empty():
+        port=queue.get()
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         result=s.connect_ex((target,port))
         if result==0:
             print("Port open", port)
             open_ports.append(port)
 
-print (f"Open ports goes as follows /n {open_ports}")
+#Defines which ports to add in the queue based on the mode
+if scanmode==1:
+   scan(allportstart,allportend)
+   portscan()
+elif scanmode==2:
+    scan(mainportstart,mainportend)
+    portscan()
+else:
+    print("This mode does not exist")
+
+
+
+print (f"Open ports goes as follows \n {open_ports}")
